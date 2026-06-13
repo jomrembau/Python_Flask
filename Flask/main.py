0aad0997dd2f9639c792908b47ec87ad92804459
jomrembau <jomir.bautista@gmail.com>
2026-06-13 09:45:38 +0200

    Flask Basic Routing

---
+++ Flask/main.py
@@ -0,0 +1,14 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return "<h1> Hello World! </h1>"
+
+@app.route("/info")
+def info():
+    return "<h1> Hello Universe! </h1>"
+
+if __name__ == "__main__":
+    app.run()
