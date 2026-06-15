3afa43499d1af6538479472cfa1f505a3f65655d
jomrembau <jomir.bautista@gmail.com>
2026-06-15 08:48:03 +0200

    template workflow

--- Flask/Template Basics/main.py
+++ Flask/Template Basics/main.py
@@ -7,7 +7,8 @@ def home():
     name = "Shea"
     letters = list(name)
     pup_dict = {"puppy_name" : "Z" }
-    return render_template("index.html", name = name, letters=letters, pup_dict = pup_dict)
+    my_list = [1,2,3,4,5]
+    return render_template("index.html", name = name, letters=letters, pup_dict = pup_dict, my_list=my_list)
 
 if __name__ == "__main__":
     app.run(debug=True)
