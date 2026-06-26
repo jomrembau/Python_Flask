ff36001184eda8a604afac433a09b66f8d6bc44e
jomrembau <jomir.bautista@gmail.com>
2026-06-26 08:05:58 +0200

    Error Pages

--- Large Project - Blog/companyblog/__init__.py
+++ Large Project - Blog/companyblog/__init__.py
@@ -3,4 +3,7 @@ from flask import Flask
 app = Flask(__name__)
 
 from companyblog.core.views import core
+from companyblog.error_pages.handlers import error_pages
+
 app.register_blueprint(core)
+app.register_blueprint(error_pages)
