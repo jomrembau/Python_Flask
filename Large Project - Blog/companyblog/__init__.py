589ce2fdc78bbfe48abc6d27332fb163463c8139
jomrembau <jomir.bautista@gmail.com>
2026-06-30 08:31:53 +0200

    User Views part 1

--- Large Project - Blog/companyblog/__init__.py
+++ Large Project - Blog/companyblog/__init__.py
@@ -21,7 +21,9 @@ login_manager.init_app(app)
 login_manager.login_view = "users.login"
 
 from companyblog.core.views import core
+from companyblog.users.views import users
 from companyblog.error_pages.handlers import error_pages
 
 app.register_blueprint(core)
+app.register_blueprint(users)
 app.register_blueprint(error_pages)
